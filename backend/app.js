473cb198b9b7f68cb8c2f0924cbd3dbf509a517a
NanyaZ <36711535+NanyaZ@users.noreply.github.com>
2018-12-16 20:40:27 -0500

    ferfsdf

--- backend/app.js
+++ backend/app.js
@@ -49,7 +49,7 @@ configRoutes(app);
 
 io.on('connection', SocketManager);
 
-server.listen(3231, () => {
+server.listen(3001, () => {
     console.log("We've now got a server!");
-    console.log("Your routes will be running on http://localhost:3231");
+    console.log("Your routes will be running on http://localhost:3001");
 });
